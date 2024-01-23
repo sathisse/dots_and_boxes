f50d8ad4a5d24724a224e88936009c46ab9b1457
scottt <tbadg@centripetalfusion.com>
2024-01-23 13:47:27 -0500

    Commit missing game_size_slider class

---
+++ lib/game_size_slider.dart
@@ -0,0 +1,51 @@
+import 'package:dots_and_boxes/utils.dart';
+import 'package:flutter/material.dart';
+
+import 'dots_and_boxes_game.dart';
+
+class GameSizeSlider extends StatefulWidget {
+  final void Function(MapEntry<int, (int, int)>) configureBoard;
+  final bool isEnabled;
+
+  const GameSizeSlider({required this.configureBoard, required this.isEnabled, super.key});
+
+  @override
+  State<GameSizeSlider> createState() => _GameSizeSliderState();
+}
+
+class _GameSizeSliderState extends State<GameSizeSlider> {
+  final dimChoices = getDimensionChoices();
+  late double sliderValue;
+
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('Dimension choices are: $dimChoices');
+    sliderValue = 4;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+        value: sliderValue,
+        max: dimChoices.length.toDouble() - 1,
+        divisions: dimChoices.length - 2,
+        label: "${dimChoices.keys.toList()[sliderValue.floor()]} dots",
+        onChanged: widget.isEnabled ? onSliderChanged : null);
+  }
+
+  onSliderChanged(double value) {
+    debugPrint("Slider tab set to ${value.round()}");
+
+    sliderValue = value;
+    var dims = dimChoices.entries
+        .where((dim) => dim.value.$1 * dim.value.$2 >= dimChoices.keys.toList()[value.floor()])
+        .first;
+    debugPrint("dims=$dims");
+    if (dims.key != numberOfDots) {
+      widget.configureBoard(dims);
+    }
+
+    setState(() {});
+  }
+}
